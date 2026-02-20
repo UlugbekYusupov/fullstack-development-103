48c5aa38fa2a665cd825bef858c7c04aead1f79f
Amir77725 <dikiydeaad@gmail.com>
2026-02-20 18:37:52 +0500

    add classwork

---
+++ app.js
@@ -0,0 +1,69 @@
+// console.log("5"+ 5); 
+// console.log("5" - 5);
+// console.log("5" * 2);
+// console.log("10" / 2);
+// console.log(5 + true);
+// console.log("10" - true);
+// console.log("5" + ture);
+// console.log(5 + null);
+// console.log(5 +  undefined);
+// console.log("5" + "5");
+
+
+// var a = "ulugbek";
+// const a = "ulugbek";
+// console.log(a);
+
+// const a = "Ulugbek";
+// console.log(a);
+
+
+// let userName 
+
+// let ageGroup5
+
+// let _internalValue
+
+// let $kind0fSpecial
+
+// challange 2
+
+// let = ("200" + 50);
+// console.log(let);
+
+// let age = 25
+// let strAge = toString(age)
+// let word = `${strAge} years old`
+// console.log(word);
+
+// let bool = "false"
+// let check = Boolean(bool)
+
+// console.log("10" - true);
+
+// challange3
+
+// const temperatureCelcius = 25;
+// console.log(temperatureCelcius);
+
+// const temperature = 25;
+// const temperatureFarenheit = (temperature * 9/5) + 32;
+// console.log(temperature + "is equal to" + temperatureFarenheit + "F");
+
+// challange 4
+
+let totalBudget = 100
+
+let exp1 = Number(prompt("expense1: "))
+let exp2 = Number(prompt("expense2: "))
+let exp3 = Number(prompt("expense3: "))
+
+totalBudget -= exp1
+totalBudget -= exp2
+totalBudget -= exp3
+
+if (totalBudget < 0) {
+    alert("You have overspent");
+}
+
+console.log(parseFloat(totalBudget));
