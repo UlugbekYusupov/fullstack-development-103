bc9490768a26594cd1e83f06e928c4cc24e4c8c4
Nur Temirov <bunurtemirov@gmail.com>
2026-02-20 18:00:01 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log()
+// Challenge 1
+console.log("5" + 5)
+console.log("5" - 5)
+console.log("5" * 5)
+console.log("5" * 5)
+console.log("10" / 2)
+console.log(5 + true)
+console.log("10" - true)
+console.log("10" + true)
+console.log(5 + null)
+console.log(5 + undefined)
+
+// Challenge 3
+const celsius = 25;
+const fahrenheit = (celsius * 9/5) + 32;
+console.log(`Temperature: ${celsius}°C`);
+console.log(`Converted: ${fahrenheit}°F`);
