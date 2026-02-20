020b8b3da16eb0072fc4034edb3cf724315b8f40
N'medova <nmedova.m@gmail.com>
2026-02-20 18:30:51 +0500

    Challange 1,2✅

---
+++ app.js
@@ -0,0 +1,28 @@
+//Challange 1
+console.log("5" + 5);
+console.log("5" - 5);
+console.log("5" * 2);
+console.log("10" / 2);
+console.log("5" + true);
+console.log("10" - true);
+console.log("5" + true);
+console.log(5 - null);
+console.log(5 + undefined);
+//Challange 2
+let num = "200" * "1" + 500
+console.log(num);
+let num2 = 25 + " years old"
+console.log(num2);
+let checking = "false"
+if (checking) {
+    console.log("This is true");
+} else {
+    console.log("This is false");
+}
+console.log("10" - true);
+
+//Challange 3
+let C = 30
+const F = (C*9/5)+32
+console.log(F); 
+
