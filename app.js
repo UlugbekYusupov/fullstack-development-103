f9fc659341aafb91a02568331b44192800103f3c
abdulxamidabrorov <abdulxamidabrorov@gmail.com>
2026-02-23 14:19:08 +0500

    Update app.js

---
+++ app.js
@@ -0,0 +1,99 @@
+// Challenge 1
+// console.log("5" + 5);
+// console.log("5" - 5);
+// console.log("5" * "2");
+// console.log("10" / "2");
+// console.log(5 + true);
+// console.log("10" - true);
+// console.log("5" + true);
+// console.log(5 + null);
+// console.log(5 + undefined);
+
+// Challenge 2
+// console.log("200" + 50);
+// console.log("25" + "", "years old")
+// console.log("false")
+// console.log("10" - true)
+
+// Challenge 3
+// const celsius = Number(prompt("Enter temperature in Celsius:"));
+
+// let fahrenheit = (celsius * 9/5) + 32;
+
+// console.log("Celsius:", celsius);
+// console.log("Fahrenheit:", fahrenheit);
+
+// Challenge 4
+// let budget = Number(prompt("Enter your total budget:"));
+// let expense1 = Number(prompt("Enter expense 1:"));
+// let expense2 = Number(prompt("Enter expens 2:"));
+// let expense3 = Number(prompt("Enter expense 3:"));
+// let totalExpenses = expense1 + expense2 + expense3;
+
+// let remaining = budget - totalExpenses;
+
+// if (remaining < 0) {
+//     console.log("Warning! You have overspent!");
+// } else {
+//     console.log("You are within your budget.");
+// }
+// console.log("Remaining money:", remaining);
+
+// Challenge 1
+// let number = 11;
+// let evenCount = 0;
+// let oddCount = 0;
+// for (let i = 1; i<= number; i++) {
+//     if (i % 2 == 0) {
+//         evenCount++;
+//     } else {
+//         oddCount++;
+//     }
+// }
+// console.log("Even numbers:", evenCount);
+// console.log("Odd number;", oddCount);
+
+// Challenge 2
+// let number = 1234;   
+// let reversed = 0;
+
+// while (number > 0) {
+
+//     let digit = number % 10;   
+//     reversed = reversed * 10 + digit;   
+//     number = Math.floor(number / 10);   
+// }
+
+// console.log(reversed);
+
+// Challenge 3
+// let number = 4321;
+// let largest = 0;
+// while (number > 0) {
+//     let digit = number % 10;
+//     if (digit > largest) {
+//         largest = digit;
+//     }
+//     number = Math.floor(number / 10)
+// }
+// console.log("Largest digit:", largest)
+
+// Challeng 4
+// let number = 1234;   
+// let sum = 0;         
+// while (number > 0) {
+//     let digit = number % 10;
+//     sum = sum + digit;
+//     number = Math.floor(number / 10);
+// }
+// console.log("Sum of digits:", sum);
+
+// Challenge 5
+// let number = 12345;
+// let count = 0;
+// while (number > 0) {
+//     let digit = number % 10;
+//     number = Math.floor(number / 10);
+//     count = count + 1;
+// }
+// console.log("Number of digits:", count);
