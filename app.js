07a6ef690f33de7ace16062084c2b4cf29cc6def
OneYusuff <masqarjanov4@gmail.com>
2026-02-24 13:19:43 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+// Task1
+// console.log("5" + 5);
+// console.log("5" - 5);
+// console.log("5" * "2");
+// console.log("10"/2);
+// console.log( 5 + true);
+// console.log("10" - true);
+// console.log("5" + true);
+// console.log(5 + null);
+// console.log(5 + undefined);
+// // Task2
+// console.log("200" + 50);
+// const age = 25
+// console.log(age + " " + "years old")
+// console.log("false")
+// console.log("10" - true)
+// // Task 3 
+// const celsius = Number(prompt("Enter temperature in Celsius:"));
+// const fahrenheit = (celsius * 9/5) + 32;
+// console.log("celsius" +" "+ "fahrenheit")
+// // We cannot change const 
+// const name = prompt("Enter a number")
+// console.log(name)
+// Task4
+let budget = prompt("Enter your total budget")
+let a = prompt("Enter first amount:")
+let b = prompt("Enter second amount:")
+let d = prompt("Enter third amount:")
+let result = budget = a + b + d
+let = remaining = budget - result
+console.log(result)
