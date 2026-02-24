dd35319ba5c8e9579f681706c938ea6648b48fa8
UlugbekYusupov <bilmadim.uz@gmail.com>
2026-02-24 17:13:09 +0500

    lecture 3 practice

---
+++ app.js
@@ -0,0 +1,50 @@
+// const user = {
+//   name: "Ulugbek",
+// };
+
+// const userr = user;
+
+// console.log(user === userr);
+
+const user = {
+  username: "Ulugbek",
+  age: 20,
+  country: "Uzbekistan",
+};
+
+const keys = Object.keys(user);
+const values = Object.values(user);
+
+// console.log(values);
+
+// for (const key in user) {
+//   const element = user[key];
+//   console.log(element);
+// }
+
+// for (const element of user) {
+//   console.log(element);
+// }
+
+// let number = Number(prompt("Sonni kiriting :"));
+// let res = 0;
+// while (number > 0){
+//     let lastDigit = number % 10;
+//     res = res * 10 + lastDigit;
+//     number = Math.floor(number / 10);
+
+// }
+// console.log(res)
+
+let usrNum = Number(prompt("give a num: "));
+let maxNum = 0;
+
+while (usrNum > 0) {
+  let lastDigit = usrNum % 10;
+  if (maxNum < lastDigit) {
+    maxNum = lastDigit;
+  }
+  usrNum = Math.floor(usrNum / 10);
+}
+
+console.log(maxNum);
