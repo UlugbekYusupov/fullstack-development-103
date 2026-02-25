9ee3250797986a4b37ad181b8ea6e992068dcc6b
Javohir Bahodirov <jbahodirov157@gmail.com>
2026-02-25 23:13:13 +0500

    Lecture 4 Homework

--- app.js
+++ app.js
@@ -516,3 +516,196 @@
 
 // sorting() 
 
+// Wrong Version ------
+// let i = 1
+// let UserInput = Number(prompt("Enter a number: "))
+
+// let EvenCounter = 0
+// let OddCounter = 0
+
+// function OddEven() {
+//     if (UserInput % 10 == 0) {
+//         FinalPartEven = UserInput / 10
+//         EvenCounter += FinalPartEven
+//     }
+//     else if (UserInput % 10 != 0) {
+//         FinalPartOdd = UserInput / 10
+//         OddCounter += FinalPart
+//     }
+
+//     console.log(EvenCounter)
+//     console.log(OddCounter)
+// }
+
+// OddEven()
+
+// Swtich Statements
+
+// let grade = prompt("Enter your grade: ")
+// let grade1 = "Distinction"
+// let grade2 = "Merit"
+// let grade3 = "Pass"
+// switch (grade) {
+//     case grade1:
+//         console.log("Wow excellent!")
+//         break
+//     case grade2:
+//         console.log("Not too bad!")
+//         break
+//     case grade3:
+//         console.log("Try harder next time!")
+//         break
+//     default:
+//         console.log("Invalid grade!")
+// }
+
+// Do while
+// This code block runs once, even though the condition is false
+
+// let count = 5
+// do {
+//     count ++
+//     console.log("Count is: " + count)
+// } while (count < 5)
+
+// for...in and for...of loops 
+
+// for in - The for...in statement iterates over all enumerable property keys (names/indices) of an object, including inherited properties from its prototype chain.
+
+// const person = {
+//     FirstName: "Jovohir",
+//     SecondName: "Bohodirov",
+//     Age: 18,
+//     IsMarried: false
+// }
+
+// for (const key in person) {
+//     console.log(key + ": " + person[key])
+// } 
+
+// for of 
+// The for...of statement iterates directly over the values of an iterable object.
+
+// const cars = ['BMW', 'AUDI', 'TESLA', 'NEXIA']
+// for (const value of cars) {
+//     console.log(value)
+// }
+
+// First exercise
+// let FirstNumber = prompt("Enter the first number: ")
+// let SecondNumber = prompt("Enter the second number: ")
+// if (FirstNumber > SecondNumber) {
+//     console.log("Larger number is " + FirstNumber)
+// }
+// else if (SecondNumber > FirstNumber) {
+//     console.log("Larger number is " + SecondNumber) 
+// }
+// else {
+//     console.log("They are equal")
+// }
+
+// Second exercise
+// let NumberArray = [3,-8,2]
+// for (let i = 0; i < NumberArray.length; i++) {
+//     if (NumberArray[i] < 0) {
+//         alert("You entered negative number (" + NumberArray[i] + ") The Sign is '-' ")
+//     }
+// }
+
+// Third exercise
+// let FirstNumber = Number(prompt("Enter the first number: "))
+// let SecondNumber = Number(prompt("Enter the second number: "))
+// let ThirdNumber = Number(prompt("Enter the third number: "))
+
+// let largest, average, smallest;
+
+// if (FirstNumber >= SecondNumber && FirstNumber >= ThirdNumber) {
+//     largest = FirstNumber
+//     if (SecondNumber >= ThirdNumber) {
+//         average = SecondNumber
+//         smallest = ThirdNumber
+//     }
+//     else {
+//         average = ThirdNumber
+//         smallest = SecondNumber
+//     }
+// }
+// else if (SecondNumber >= FirstNumber && SecondNumber >= ThirdNumber) {
+//     largest = SecondNumber
+//     if (FirstNumber >= ThirdNumber) {
+//         average = FirstNumber
+//         smallest = ThirdNumber
+//     }
+//     else {
+//         average = ThirdNumber
+//         smallest = FirstNumber
+//     }
+// }
+// else {
+//     largest = ThirdNumber
+//     if (FirstNumber >= SecondNumber) {
+//         average = FirstNumber
+//         smallest = SecondNumber
+//     }
+//     else {
+//         average = SecondNumber
+//         smallest = FirstNumber
+//     }
+// }
+
+// console.log("Largest: " + largest + ", Average: " + average + ", Smallest: " + smallest)
+
+// 4-exercise
+// for (let i = 0; i <= 15; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i + " is even")
+//     }
+//     else {
+//         console.log(i + " is odd")
+//     }
+// }
+
+// 5-exercise
+// for (let i = 0; i <= 100; i++) {
+//     if (i % 3 == 0) {
+//         console.log("Fizz")
+//     }
+//     else if (i % 5 == 0) {
+//         console.log("Buzz")
+//     }
+//     else if (i % 3 == 0 && i % 5 == 0) {
+//         console.log("FizzBuzz")
+//     }
+//     else {
+//         console.log(i)
+//     }
+// }
+
+// 6-exercise 
+// Could not accomplish this task
+
+// 7-exercise
+// let Marks = [80,77,88,95,68]
+// let sum = 0
+
+// for (let i = 0; i < Marks.length; i++) {
+//     sum += Marks[i]
+//     sum = sum / Marks.length
+// }
+// console.log("Their average mark is: " + sum)
+
+// if (sum <= 60) {
+//     console.log("Grade: F")
+// }
+// else if (sum > 60 && sum <=70) {
+//     console.log("Grade: D")
+// }
+// else if (sum > 70 && sum <= 80) {
+//     console.log("Grade: C")
+// }
+// else if (sum > 80 && sum <=90) {
+//     console.log("Grade: 90")
+// }
+// else if (sum > 90 && sum <= 100) {
+//     console.log("Grade: A")
+// }
