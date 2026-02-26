3410b9f0ca68ddedccbf039dc268777280a76a3c
amanovKamronbek <amanovkamron999@gmail.com>
2026-02-26 14:28:21 +0500

    secondHomework

--- app.js
+++ app.js
@@ -1,38 +1,67 @@
-// Challenge 1 - done 
+//  Challenge 1
+// let number = Number(prompt("Enter the number: "))
+// let evenCounter = 0;
+// let oddCounter = 0
+
+// for(let i = 1; i < number; i++) {
+//     if (i % 2 == 0) {
+//         evenCounter++
+//     } 
+//     if (i % 2) {
+//         oddCounter++
+//     }
+// }
+
+// console.log(evenCounter)
+// console.log(oddCounter)
 
 // Challenge 2 
-let number = "200" * 1 // Converting 200 to number using *
-console.log(number + 50)
+// let number = Number(prompt("Enter the number: "))
 
-let age = 25
-let result = 25 + " years old"
-console.log(result, "| Type: " + typeof result)
+// let result = 0
+// while (number > 0) {
+//     let lastNum = number % 10
+//     result = result * 10 + lastNum
+//     number = Math.floor(number / 10)
+// }
 
-if ('false' == true) console.log(true) // Program tries to convert false(string) to Number and it returns NaN and true also will be converted to Number which is 1. If we check Nan == 1 it gives us false so thats why console.log showing false
-    else console.log(false) 
+// console.log(result)
 
-console.log("10" - true) // Program converts 10 to Number cuz we r using - and true equals to 1 false to 0. Now simple math 10 - 1 = 9
 
-//  ============== Challenge 3 ============== 
-const temp = 10
-// temp = 30 // This returns TypeError because when we use CONST we cant reasign it.
-const fahr = (temp * 9/5) + 32
-console.log(fahr)
 
-// Secon method
-const tempInput = Number(prompt('Enter the celcius: '))
-const fahr2 = (tempInput * 9/5) + 32
-console.log(fahr2)
+// Challenge 3
+// let number = Number(prompt("Enter the number: "))
+// let largestNumber = 0
 
-//  ============== Challenge 4 ============== 
-let totalBudget = Number(prompt('Enter your total budget: ')) // 100
+// while(number > 0) {
+//     let lastNum = number % 10 // 1234 -> 4
+//     if (lastNum > largestNumber) largestNumber = lastNum
 
-let firstExpense = Number(prompt('Enter first amount:'))
-let secondExpense = Number(prompt('Enter second amount:'))
-let thirdExpense = Number(prompt('Enter third amount:'))
+//     number = Math.floor(number / 10)
+// }
 
-let totalExpense = firstExpense + secondExpense + thirdExpense 
+// console.log(largestNumber)
 
-if (totalExpense > totalBudget) alert('Error! You have overspent.') 
-    else alert(`Success! Current budget: ${totalBudget - (firstExpense + secondExpense + thirdExpense)}`)
+// Challenge 4
+// let number = Number(prompt("Enter the number: "))
+// let sum = 0;
 
+// while(number > 0) {
+//     let lastNum = number % 10
+//     sum += lastNum
+//     number = Math.floor(number / 10)
+// }
+
+// console.log(sum)
+
+
+// Challenge 5
+let number = Number(prompt("Enter the number: "))
+let count = 0
+
+while (number > 0) {
+    number = Math.floor(number / 10);
+    count += 1
+}
+
+console.log(count)
