e2d5c59eaeaf88815884fd245183fc66550685b8
Mukhammadaziz <muhammadazizabdulhayev67@gmail.com>
2026-02-26 13:57:12 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,36 +1,90 @@
 
-console.log("5" + 5 );
-console.log("5" - 5 );
-console.log("5" * "2" );
-console.log("10" / 2 );
-console.log(5 + true );
-console.log("10" - true );
-console.log("5" + true );
-console.log(5 + null );
-console.log(5 + undefined);
+
+// --------------------OPERATORS------------------------------
+
+// Challange 1
+
+
+let n = Number(prompt("Enter the number: "));
+
+let even = 0
+let odd = 0
+
+for (let i = 1 ; i<= n ; i++){
+    if (i % 2 === 0) {
+        even++;
+    } else {
+        odd++;
+    }
+}
+
+console.log("Even number: ", even)
+console.log("Odd number: " , odd)
+
 
 
 // Challange 2
-let num1 = Number("200");
-console.log(num1 + 50);
+
+let num = parseInt(prompt("Enter the number: "));
+
+let reversed = 0;
+
+while (num > 0) {
+    let lastDigit = num % 10;
+    reversed = reversed * 10 + lastDigit;
+    num = Math.floor(num / 10);
+}
+
+console.log(reversed);
 
 
 // Challange 3
-const celsuis = 20
-const fahrenheit = (celsuis * 9/5) + 32 
-console.log("Celsius:" , celsuis)
-console.log("Fahrenheit: " , fahrenheit)
 
+let maxNum = Number(prompt("Enter the number: "))
+
+let max = 0
+
+while(maxNum > 0){
+    let lastDigit = maxNum % 10
+    if (maxNum < lastDigit){
+        maxNum = lastDigit
+    }
+    maxNum = Math.floor(maxNum / 10);
+}
+
+console.log(maxNum);
 
 // Challange 4
-let budget = Number(prompt("Enter the budget: "));
 
-let expense1 = Number(prompt("Enter the expense 1: "));
-let expense2 = Number(prompt("Enter the expense 2: "));
-let expense3 = Number(prompt("Enter the expense 3: "));
+let number = Number(prompt("Enter the number: "))
 
-let allExpenses = expense1 + expense2 + expense3
+let sum = 0
 
-let result = budget - allExpenses
+while (number > 0) {
+    let lastDigit = number % 10
+    sum = sum + lastDigit
+    number = Math.floor(number / 10);
+}
 
-console.log("Result : " , result)
+console.log("Sum of the numbers: " , sum);
+
+// Challange 5 
+
+let num1 = 12345;   
+let count = 0;
+
+if (num1 === 0) {
+  count = 1;
+} else {
+
+  if (num1 < 0) {
+    num1 = -num1;
+  }
+
+  while (num1 > 0) {
+    num1 = Math.floor(num1 / 10);
+    count++;
+  }
+}
+
+console.log("Digits:", count);
