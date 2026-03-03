b8d2ddcf526769e739203649bf59c023a02d1b7e
amanovKamronbek <amanovkamron999@gmail.com>
2026-03-03 15:12:02 +0500

    Lecture 5 Done

--- app.js
+++ app.js
@@ -1,67 +1,164 @@
-//  Challenge 1
-// let number = Number(prompt("Enter the number: "))
-// let evenCounter = 0;
-// let oddCounter = 0
+// =========================== Challenge 1 ===========================
 
-// for(let i = 1; i < number; i++) {
-//     if (i % 2 == 0) {
-//         evenCounter++
-//     } 
-//     if (i % 2) {
-//         oddCounter++
+// function 1
+// function greet(name) {
+//     console.log(`Hello, ${name}`)
+// }
+
+// greet('Kamronbek')
+
+// function 2
+// let greet = function (name) {
+//     console.log(`Hello, ${name}`)
+// }
+
+// greet('Kamronbek')
+
+// function 3
+// let greet = (name) => {
+//     console.log(`Hello, ${name}`)
+// }
+
+// greet('Kamronbek')
+
+// function 4
+// (function(name){
+//     console.log(`Hello, ${name}`)
+// }('Kamronbek'))
+
+// =========================== Challenge 2 =========================== 
+// let count = 0;
+
+// function isPrime(n) {
+//     for(let i = 1; i < n; i++) {
+//         if(n % i == 0) {
+//             count += 1
+//         }
 //     }
 // }
 
-// console.log(evenCounter)
-// console.log(oddCounter)
 
-// Challenge 2 
-// let number = Number(prompt("Enter the number: "))
+// isPrime(17)
 
-// let result = 0
-// while (number > 0) {
-//     let lastNum = number % 10
-//     result = result * 10 + lastNum
-//     number = Math.floor(number / 10)
+// if(count <= 2) console.log('prime num')
+//     else console.log('not prime')
+
+// =========================== Challenge 3 =========================== 
+// let count = 0
+// let number = Number(prompt('Enter number: '))
+
+// function countDigits(n) { 
+//     while (n > 0) {
+//     n = Math.floor(n / 10);
+//     count += 1
+// }
 // }
 
-// console.log(result)
+// countDigits(number)
+// console.log(count)
 
+// =========================== Challenge 4 =========================== 
+// function isPalindrome(n) {
+//     let firstNumber = n
+//     let reversedNumber = 0
 
+//     while (n > 0) {
+//         let digit = n % 10          
+//         reversedNumber = reversedNumber * 10 + digit
+//         n = Math.floor(n / 10)  
+//     }
 
-// Challenge 3
-// let number = Number(prompt("Enter the number: "))
-// let largestNumber = 0
-
-// while(number > 0) {
-//     let lastNum = number % 10 // 1234 -> 4
-//     if (lastNum > largestNumber) largestNumber = lastNum
-
-//     number = Math.floor(number / 10)
+//     return firstNumber === reversedNumber
 // }
 
-// console.log(largestNumber)
+// console.log(isPalindrome(121))
+// console.log(isPalindrome(321))
 
-// Challenge 4
-// let number = Number(prompt("Enter the number: "))
-// let sum = 0;
+// =========================== Challenge 5 =========================== 
+// function isArmstrong(n) {
+//     if (n < 0) return false
 
-// while(number > 0) {
-//     let lastNum = number % 10
-//     sum += lastNum
-//     number = Math.floor(number / 10)
+//     let number = n
+//     let temp = n
+//     let digits = 0
+//     let sum = 0
+
+//     while (temp > 0) {
+//         digits++
+//         temp = Math.floor(temp / 10)
+//     }
+
+//     temp = n
+
+//     while (temp > 0) {
+//         let digit = temp % 10
+//         sum += digit ** digits
+//         temp = Math.floor(temp / 10)
+//     }
+
+//     return sum === number
 // }
 
-// console.log(sum)
+// console.log(isArmstrong(153)) 
+// console.log(isArmstrong(133)) 
 
+// =========================== Challenge 6 =========================== 
 
-// Challenge 5
-let number = Number(prompt("Enter the number: "))
-let count = 0
+// function createCounter(start) {
+//     let count = start
 
-while (number > 0) {
-    number = Math.floor(number / 10);
-    count += 1
-}
+//     return {
+//         increment() {
+//             count++
+//         },
+//         decrement() {
+//             count--
+//         },
+//         getCount() {
+//             return count
+//         }
+//     }
+// }
+// const counter = createCounter(5)
 
-console.log(count)
+// counter.increment()
+// counter.increment()
+// counter.decrement()
+
+// console.log(counter.getCount()) 
+
+// =========================== Challenge 7 =========================== 
+
+// function once(fn) {
+//     let called = false
+
+//     return function() {
+//         if (!called) {
+//             called = true
+//             return fn()
+//         }
+//     }
+// }
+
+// function greet() {
+//     console.log("Hello!")
+// }
+
+// const sayHello = once(greet)
+
+// sayHello()
+// sayHello() 
+// sayHello() 
+
+// =========================== Challenge 8 =========================== 
+
+// function lazyAdder(a) {
+//     return function(b) {
+//         return a + b
+//     }
+// }
+
+// const add5 = lazyAdder(5)
+
+// console.log(add5(10)) 
+// console.log(add5(20)) 
