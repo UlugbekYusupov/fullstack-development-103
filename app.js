414910e9756b2d50e892832e9a42d63c4e1dc6a3
abdurashidovaxrorbek <abdurashidovaxrorbek244@gmail.com>
2026-03-05 10:39:23 +0500

    lecture 6

--- app.js
+++ app.js
@@ -1,122 +1,161 @@
-// Challane 1
-// Function Declaration
-function greet(name) {
-  return "Hello, " + name + "!";
-}
-console.log(greet("Ali"));
 
-//  Function Expression
-const greet2 = function(name) {
-  return "Hello, " + name + "!";
-};
-console.log(greet2("Vali"));
+// Challange 1
 
-//  Arrow Function
-const greet3 = (name) => {
-  return "Hello, " + name + "!";
-};
-console.log(greet3("Hasan"));
+function first(arr, n) {
+  if (arr.length === 0) return [];
 
-//  IIFE
-const greet4 = (function() {
-  return function(name) {
-    return "Hello, " + name + "!";
-  };
-})();
-console.log(greet4("Husan"));
-
-//  Function Constructor
-const greet5 = new Function("name", "return 'Hello, ' + name + '!';");
-console.log(greet5("Aziza"));
-
-// Challang2 2
-function isPrime(n) {
-  if (n <= 1) {
-    return false;
+  if (n == null) {
+    return arr[0];
   }
 
-  for (var i = 2; i < n; i++) {
-    if (n % i === 0) {
-      return false;
-    }
+  if (n < 0) {
+    return n;
   }
 
-  return true;
+  return arr.slice(0, n);
 }
 
-console.log(isPrime(7));  
-console.log(isPrime(10));
+// Test
+console.log(first([7, 9, 0, -2]));       // 7
+console.log(first([],3));                // []
+console.log(first([7, 9, 0, -2],3));     // [7,9,0]
+console.log(first([7, 9, 0, -2],6));     // [7,9,0,-2]
+console.log(first([7, 9, 0, -2],-3));    // -3
+
+
+// Challange 2
+
+function last(arr, n) {
+  if (n == null) {
+    return arr[arr.length - 1];
+  }
+
+  return arr.slice(Math.max(arr.length - n, 0));
+}
+
+// Test
+console.log(last([7, 9, 0, -2]));       // -2
+console.log(last([7, 9, 0, -2],3));     // [9,0,-2]
+console.log(last([7, 9, 0, -2],6));     // [7,9,0,-2]
 
 
 // Challane 3
 
-function countDigits(n) {
-  var num = Math.abs(n);
-  var str = num.toString();
-  return str.length;
+let myColor = ["Red", "Green", "White", "Black"];
+
+let result = myColor.join(",");
+
+console.log(result);
+
+
+// Challane 4
+
+function addDash(num) {
+  let str = num.toString();
+  return str.split("").join("-");
 }
 
-console.log(countDigits(12345));
-console.log(countDigits(-9876));
-
-// Challange 4
-
-function isPalindrome(n) {
-  var str = n.toString();
-  var reversed = str.split("").reverse().join("");
-
-  if (str === reversed) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-console.log(isPalindrome(121));
-console.log(isPalindrome(123));
+console.log(addDash(25468));
 
 // Challange 5
 
-function isArmstrong(n) {
-  var str = n.toString();
-  var power = str.length;
-  var sum = 0;
+let numbers = [-3, 8, 7, 6, 5, -4, 3, 2, 1];
 
-  for (var i = 0; i < str.length; i++) {
-    var digit = Number(str[i]);
-    sum = sum + Math.pow(digit, power);
+numbers.sort(function(a, b) {
+  return a - b;
+});
+
+console.log(numbers);
+
+// Challange 6
+
+
+let items = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+
+let counter = {};
+let mostItem;
+let mostCount = 0;
+
+for (let i = 0; i < items.length; i++) {
+
+  let element = items[i];
+
+  if (counter[element] == null) {
+    counter[element] = 1;
+  } else {
+    counter[element]++;
   }
 
-  return sum === n;
+  if (counter[element] > mostCount) {
+    mostCount = counter[element];
+    mostItem = element;
+  }
 }
 
-console.log(isArmstrong(153));
-console.log(isArmstrong(123));
+console.log(mostItem + " (" + mostCount + " times)");
 
 
-// Challane 6
+// Challane 7
 
-function createCounter(start) {
-  var count = start;
 
-  return {
-    increment: function() {
-      count++;
-    },
-    decrement: function() {
-      count--;
-    },
-    getCount: function() {
-      return count;
-    }
-  };
+let text = "The Quick Brown Fox";
+let resulttt = "";
+
+for (let i = 0; i < text.length; i++) {
+
+  let letter = text[i];
+
+  if (letter === letter.toUpperCase()) {
+    resulttt += letter.toLowerCase();
+  } else {
+    resulttt += letter.toUpperCase();
+  }
+
 }
 
-var counter = createCounter(5);
+console.log(result);
 
-counter.increment();
-counter.increment();
-console.log(counter.getCount());
 
-counter.decrement();
-console.log(counter.getCount());
+// Challange 8
+
+let colors = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"];
+let suffix = ["th","st","nd","rd"];
+
+for (let i = 0; i < colors.length; i++) {
+
+  let number = i + 1;
+  let ending;
+
+  if (number == 1) {
+    ending = suffix[1];
+  } else if (number == 2) {
+    ending = suffix[2];
+  } else if (number == 3) {
+    ending = suffix[3];
+  } else {
+    ending = suffix[0];
+  }
+
+  console.log(number + ending + " choice is " + colors[i]);
+}
+
+
+// Challane 9
+
+
+let array1 = [1,0,2,3,4];
+let array2 = [3,5,6,7,8,13];
+
+let resultt = [];
+
+let maxLength = Math.max(array1.length, array2.length);
+
+for (let i = 0; i < maxLength; i++) {
+
+  let value1 = array1[i] || 0;
+  let value2 = array2[i] || 0;
+
+  resultt.push(value1 + value2);
+}
+
+console.log(result);
