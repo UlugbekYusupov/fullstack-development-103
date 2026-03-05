8cc8a8d322a8d7ea4244c9c10404b7eaca21e35b
Javohirborotov2006 <javohirbek782@gmail.com>
2026-03-05 09:35:12 +0500

    Lecture 6 Done

--- app.js
+++ app.js
@@ -1,26 +1,164 @@
 //1
-// function greet(name) {
-//     return "Hello, " + name + "!";
-// }
+function first(arr, n) {
 
-// let greet2 = function(name) {
-//     return "Hello, " + name + "!";
-// };
+  if (arr.length === 0) return [];
 
-// let greet3 = (name) => {
-//     return "Hello, " + name + "!";
-// };
+  if (n == null) return arr[0];
 
-// let greet4 = name => "Hello, " + name + "!";
+  if (n < 0) return n;
 
-// function greet5(name) {
-//     return `Hello, ${name}!`;
-// }
-
-// console.log(greet("Ali"));
-// console.log(greet2("Ali"));
-// console.log(greet3("Ali"));
-// console.log(greet4("Ali"));
-// console.log(greet5("Ali"));
+  return arr.slice(0, n);
+}
+console.log(first([7, 9, 0, -2]));      
+console.log(first([], 3));              
+console.log(first([7, 9, 0, -2], 6));   
+console.log(first([7, 9, 0, -2], -3));  
 
 //2
+function last(arr, n) {
+
+  if (n == null) return arr[arr.length - 1];
+
+  if (n > arr.length) return arr;
+
+  return arr.slice(arr.length - n);
+}
+
+
+console.log(last([7, 9, 0, -2]));      
+console.log(last([7, 9, 0, -2], 3));   
+console.log(last([7, 9, 0, -2], 6));
+
+//3
+let myColor = ["Red", "Green", "White", "Black"];
+
+let result = myColor.join(",");
+
+console.log(result); 
+
+//4
+function addDash(num) {
+
+  let str = num.toString();   
+  let result = "";
+
+  for (let i = 0; i < str.length; i++) {
+
+    result += str[i];
+
+    if (i !== str.length - 1) {
+      result += "-";
+    }
+
+  }
+
+  return result;
+}
+
+console.log(addDash(25468)); 
+
+//5
+function sortArray(arr) {
+
+  return arr.sort(function(a, b) {
+    return a - b;
+  });
+
+}
+var arr1 = [ -3, 8, 7, 6, 5, -4, 3, 2, 1 ];
+
+console.log(sortArray(arr1));
+
+//6
+function mostFrequent(arr) {
+
+  let count = {};
+  let max = 0;
+  let result;
+
+  for (let i = 0; i < arr.length; i++) {
+
+    let item = arr[i];
+
+    count[item] = (count[item] || 0) + 1;
+
+    if (count[item] > max) {
+      max = count[item];
+      result = item;
+    }
+
+  }
+
+  return result + " ( " + max + " times )";
+}
+
+var arr1=[3,'a','a','a',2,3,'a',3,'a',2,4,9,3];
+
+console.log(mostFrequent(arr1));
+
+//7
+function swapCase(str) {
+
+  let result = "";
+
+  for (let i = 0; i < str.length; i++) {
+
+    if (str[i] === str[i].toUpperCase()) {
+      result += str[i].toLowerCase();
+    } 
+    else {
+      result += str[i].toUpperCase();
+    }
+
+  }
+
+  return result;
+}
+console.log(swapCase("The Quick Brown Fox"));
+
+//8
+let color = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"];
+let o = ["th","st","nd","rd"];
+
+for (let i = 0; i < color.length; i++) {
+
+  let num = i + 1;
+  let ordinal;
+
+  if (num === 1) {
+    ordinal = num + o[1];
+  } 
+  else if (num === 2) {
+    ordinal = num + o[2];
+  } 
+  else if (num === 3) {
+    ordinal = num + o[3];
+  } 
+  else {
+    ordinal = num + o[0];
+  }
+
+  console.log(ordinal + " choice is " + color[i]);
+}
+
+//9
+function sumArrays(a1, a2) {
+
+  let result = [];
+  let maxLength = Math.max(a1.length, a2.length);
+
+  for (let i = 0; i < maxLength; i++) {
+
+    let val1 = a1[i] || 0;
+    let val2 = a2[i] || 0;
+
+    result.push(val1 + val2);
+  }
+
+  return result;
+}
+
+let array1 = [1,0,2,3,4];
+let array2 = [3,5,6,7,8,13];
+
+console.log(sumArrays(array1, array2));
