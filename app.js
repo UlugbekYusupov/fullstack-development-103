3ad4d72d211acfceb7ff91ed2dcefbcd60848f90
xasan2605 <xasanxujayev8@gmail.com>
2026-03-05 14:15:18 +0500

    Lecture 6 app.js

--- app.js
+++ app.js
@@ -102,71 +102,214 @@
 
 // console.log("Number of digits:", count);
                                 // 1
-let num1 = 10;
-let num2 = 25;
-if (num1>num2){
-    alert("The largest number is "+ num1);
-} else if (num2 > num1){
-    alert("The largest number is "+ num2);
-} else {
-    alert("Both  numbers are equal");
+// let num1 = 10;
+// let num2 = 25;
+// if (num1>num2){
+//     alert("The largest number is "+ num1);
+// } else if (num2 > num1){
+//     alert("The largest number is "+ num2);
+// } else {
+//     alert("Both  numbers are equal");
+// }
+
+//                                 // 2
+// let a = 3
+// let b = -7
+// let c = 2
+// let product = a * b * c
+// if (product > 0){
+//     alert("The sign is +");
+// } else if (product < 0){
+//     alert("The sign is -")
+// } else{
+//     alert("The sign is 0")
+// } 
+//                                 // 3
+// let a1 = 0 
+// let b1 = -1
+// let c1 = 4 
+// let product1 = [c1, b1, a1]
+// alert(product1)
+//                                 // 4
+// for (let i = 0; i <= 15; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i + " is even");
+//     } else {
+//         console.log(i + " is odd");
+//     }
+// }
+//                                  // 5
+// for (let i = 1; i <= 100; i++) {
+
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log("FizzBuzz");
+//     } 
+//     else if (i % 3 === 0) {
+//         console.log("Fizz");
+//     } 
+//     else if (i % 5 === 0) {
+//         console.log("Buzz");
+//     } 
+//     else {
+//         console.log(i);
+//     }
+
+// }
+
+//                                 // 6
+
+// for (let num = 100; num <= 999; num++) {
+
+//     let hundreds = Math.floor(num / 100);
+//     let tens = Math.floor((num % 100) / 10);
+//     let ones = num % 10;
+
+//     let sum = (hundreds ** 3) + (tens ** 3) + (ones ** 3);
+
+//     if (sum === num) {
+//         console.log(num);
+//     }
+// }
+Challenge1
+
+function FirstElements(array, n = 1) {
+    return array.slice(0, n);
 }
 
-                                // 2
-let a = 3
-let b = -7
-let c = 2
-let product = a * b * c
-if (product > 0){
-    alert("The sign is +");
-} else if (product < 0){
-    alert("The sign is -")
-} else{
-    alert("The sign is 0")
-} 
-                                // 3
-let a1 = 0 
-let b1 = -1
-let c1 = 4 
-let product1 = [c1, b1, a1]
-alert(product1)
-                                // 4
-for (let i = 0; i <= 15; i++) {
-    if (i % 2 === 0) {
-        console.log(i + " is even");
+const arr = [1,2,3,4,5];
+
+console.log(FirstElements(arr));
+console.log(FirstElements(arr,3));
+console.log(FirstElements(arr,10));
+
+Challnge2
+
+function SecondElements(array, n = 1) {
+    return array.slice(-n);
+}
+
+const arr = [1,2,3,4,5];
+
+console.log(SecondElements(arr));
+console.log(SecondElements(arr,3));
+
+challenge3
+
+const fruits = ["apple","banana","orange","grape"];
+const result1 = fruits.join();
+console.log(result1);
+
+const result2 = fruits.join(" - ");
+console.log(result2);
+
+const result3 = fruits.join("");
+console.log(result3);
+
+function joinarr(array, separator = ",") {
+    return array.join(separator)
+}
+
+console.log(joinarr(fruits));
+console.log(joinarr(fruits, " "));
+console.log(joinarr(fruits, " | "));
+
+challenge4
+
+const input = 25468;
+const result = insertdash(input);
+console.log(result);
+
+function insertdash(input) {
+    const str = input.toString();
+    return str.split("").join("-")
+}
+
+console.log(insertdash(24568));
+
+
+challenge5
+
+var arr1 = [-3,8,7,6,5,-4,3,2,1];
+
+var sortedarr = arr1.sort(function(a,b) {
+    return a - b;
+})
+
+console.log(sortedarr);
+challenge6
+
+var arr1 = [3, "a", "a", "a", 2, 3, "a", 3, "a", 2, 4, 9, 3];
+
+function findmostfrequent(arr) {
+    var frequency = {};
+    var maxcount = 0;
+    var mostfrequentitem;
+
+    for (var i = 0; i < arr.length; i++) {
+        var item = arr[i];
+        frequency[item] = (frequency[item]  0) + 1
+
+    if (frequency[item] > maxcount) {
+        maxcount = frequency[item];
+        mostfrequentitem = item;
+    }   
+    }
+
+    return {
+        item: mostfrequentitem,
+        count: maxcount
+    };
+}
+
+var result = findmostfrequent(arr1);
+
+console.log(result.item + "(" + result.count + "times)");
+
+challenge7
+
+function swapCase(str) {
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    let c = str[i];
+    if (c == c.toUpperCase()) {
+      result += c.toLowerCase();
     } else {
-        console.log(i + " is odd");
+      result += c.toUpperCase();
     }
-}
-                                 // 5
-for (let i = 1; i <= 100; i++) {
-
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log("FizzBuzz");
-    } 
-    else if (i % 3 === 0) {
-        console.log("Fizz");
-    } 
-    else if (i % 5 === 0) {
-        console.log("Buzz");
-    } 
-    else {
-        console.log(i);
-    }
-
+  }
+  return result;
 }
 
-                                // 6
+console.log(swapCase("The Quick Brown Fox"));
 
-for (let num = 100; num <= 999; num++) {
+challenge8
 
-    let hundreds = Math.floor(num / 100);
-    let tens = Math.floor((num % 100) / 10);
-    let ones = num % 10;
+let color = ["blue","green","red","orange","violet","indigo","yellow"];
+let suffix = ["th","st","nd","rd"];
 
-    let sum = (hundreds ** 3) + (tens ** 3) + (ones ** 3);
+color.map((item, index) => {
+  let num = index + 1;
+  if (num === 1) {
+    console.log(num + suffix[1] + " " + item);
+  } else if (num === 2) {
+    console.log(num + suffix[2] + " " + item);
+  } else if (num === 3) {
+    console.log(num + suffix[3] + " " + item);
+  } else {
+    console.log(num + suffix[0] + " " + item);
+  }
+});
 
-    if (sum === num) {
-        console.log(num);
-    }
+challenge9
+
+let arr1 = [1, 0, 2, 3, 4];
+let arr2 = [3, 5, 6, 7, 8, 13];
+let result = [];
+
+let maxLength = Math.max(arr1.length, arr2.length);
+
+for (let i = 0; i < maxLength; i++) {
+  let val1 = arr1[i]  0;  
+  let val2 = arr2[i] || 0;  
+  result[i] = val1 + val2;
 }
